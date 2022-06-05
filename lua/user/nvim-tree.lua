7ff2a6baea1772cdd820c085402fabf18b774be7
Christian Döring <doering.christian.2001@gmx.de>
2022-06-05 13:22:19 +0200

    Nvim-Tree Improvements.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -13,7 +13,18 @@ nvim_tree.setup {
                 { key = "t", action = "tabnew" },
                 { key = "h", action = "close_node" },
                 { key = "<BS>", action = "dir_up" },
+                { key = "<CR>", action = "cd" },
             },
         },
     },
 }
+
+-- Autoclose
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})

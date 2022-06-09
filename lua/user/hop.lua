875cf96cb9ffa4e69b06073362103ac74e7141ab
Döring Christian <christian.doering@tum.de>
2022-06-09 10:05:30 +0200

    Hop highligt changes

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -3,7 +3,13 @@ if not ok then
     return
 end
 
+-- Hop highlight
+vim.cmd[[hi! link HopNextKey DiagnosticError]]
+vim.cmd[[hi! link HopNextKey1 DiagnosticWarn]]
+vim.cmd[[hi! link HopNextKey2 DiagnosticInfo]]
+
 hop.setup {
     keys = "wertzuiopghyxcvbnmalskdjf",
     extend_visual = true,
 }
+

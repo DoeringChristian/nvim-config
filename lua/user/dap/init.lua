c3c94ab802782e86191799af1850e918be82e00f
Döring Christian <christian.doering@tum.de>
2022-06-11 21:46:07 +0200

    Tried DAP.

--- lua/user/dap/init.lua
+++ lua/user/dap/init.lua
@@ -36,6 +36,29 @@ local function configure()
     vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
     vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
     vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+
+    dap.adapters.lldb = {
+        type = 'executable',
+        command = '/usr/bin/lldb-vscode',
+        name = 'lldb',
+    }
+
+    dap.configurations.cpp = {
+        {
+            name = 'Launch',
+            type = 'lldb',
+            request = 'launch',
+            program = function()
+                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+            args = {},
+        }
+    }
+
+    dap.configurations.rust = dap.configurations.cpp
+
 end
 
 local function configure_exts()

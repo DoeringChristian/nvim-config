e522bb315f469f8360f9984c07115ebb798757fa
Döring Christian <christian.doering@tum.de>
2022-06-12 07:22:14 +0200

    Fixed rust-analyzer server name but still procmacros are not expanded.

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -26,7 +26,7 @@ lsp_installer.on_server_ready(function(server)
         opts = vim.tbl_deep_extend("force", pyright_opts, opts)
     end
 
-    if server.name == "rust_analyzer" then
+    if server.name == "rust-analyzer" then
         local rust_analyzer_opts = require("user.lsp.settings.rust_analyzer")
         opts = vim.tbl_deep_extend("force", rust_analyzer_opts, opts)
     end
--- lua/user/lsp/settings/rust_analyzer.lua
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -2,7 +2,11 @@ return {
     settings = {
         ["rust-analyzer"] = {
             assist = {
+                importGranularity = "module",
                 importPrefix = "self",
+            },
+            cargo = {
+                loadOutDirsFromCheck = true
             },
             procMacro = {
                 enable = true

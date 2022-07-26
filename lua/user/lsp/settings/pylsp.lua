5f7965bcbc565bae52d9fe9c8f0a5cf224ca4770
Döring Christian <doeringc@pop-os.localdomain>
2022-07-26 17:49:34 +0200

    disabled import sort

--- lua/user/lsp/settings/pylsp.lua
+++ lua/user/lsp/settings/pylsp.lua
@@ -6,7 +6,7 @@ return {
                 pyflakes = { enabled = false },
                 pycodestyle = { enabled = false },
                 jedi_completion = { fuzzy = true },
-                pyls_isort = { enabled = true },
+                pyls_isort = { enabled = false },
                 pylsp_mypy = { enabled = true },
             },
         },

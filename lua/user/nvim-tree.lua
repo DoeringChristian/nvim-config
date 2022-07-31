8eadf34ffa20a06f7d299780dbf2a5256ffd1fa8
Döring Christian <christian.doering@um.de>
2022-07-31 20:13:06 +0200

    had to disable autoclose for nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -20,11 +20,14 @@ nvim_tree.setup {
 }
 
 -- Autoclose
+--[[
 vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+            print("quit")
+            vim.cmd "quit"
+        end
     end
-  end
 })
+]] --

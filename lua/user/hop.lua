3049ab720ddf255d1730f6315ea32b339fc7fa1a
Döring Christian <christian.doering@um.de>
2022-08-16 19:13:15 +0200

    reset hop highlights

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -4,12 +4,12 @@ if not ok then
 end
 
 -- Hop highlight
---vim.cmd [[hi! link HopNextKey DiagnosticError]]
---vim.cmd [[hi! link HopNextKey1 DiagnosticError]]
---vim.cmd [[hi! link HopNextKey2 DiagnosticWarn]]
-vim.cmd [[hi! link HopNextKey Error]]
-vim.cmd [[hi! link HopNextKey1 Error]]
-vim.cmd [[hi! link HopNextKey2 String]]
+vim.cmd [[hi! link HopNextKey DiagnosticError]]
+vim.cmd [[hi! link HopNextKey1 DiagnosticError]]
+vim.cmd [[hi! link HopNextKey2 DiagnosticWarn]]
+--vim.cmd [[hi! link HopNextKey lualine_a_normal]]
+--vim.cmd [[hi! link HopNextKey1 lualine_a_normal]]
+--vim.cmd [[hi! link HopNextKey2 String]]
 
 hop.setup {
     keys = "wertzuiopghyxcvbnmalskdjf",

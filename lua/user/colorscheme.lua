4c123eddbb27b569d35559e247f17e8d09413e39
Doering Christian <christian.doering@tum.de>
2022-10-16 17:29:46 +0200

    Colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,31 +1,37 @@
--- Gruvbox-Baby --
+local colorscheme = 'gruvbox-material'
 
---vim.g.gruvbox_baby_background_color = "dark"
---vim.cmd([[colorscheme gruvbox-baby]])
 
--- Gruvbox-Material
-vim.cmd [[
-if has('termguicolors')
-    set termguicolors
-endif
+local configs = {
+    ["gruvbox-baby"] = function()
+        vim.g.gruvbox_baby_background_color = "dark"
+        vim.cmd([[colorscheme gruvbox-baby]])
+    end,
+    ["gruvbox-material"] = function()
+        vim.cmd [[
+        if has('termguicolors')
+            set termguicolors
+        endif
 
-" For dark version.
-set background=dark
+        " For dark version.
+        set background=dark
 
-" Set contrast.
-" This configuration option should be placed before `colorscheme gruvbox-material`.
-" Available values: 'hard', 'medium'(default), 'soft'
-let g:gruvbox_material_background = 'hard'
+        " Set contrast.
+        " This configuration option should be placed before `colorscheme gruvbox-material`.
+        " Available values: 'hard', 'medium'(default), 'soft'
+        let g:gruvbox_material_background = 'hard'
 
-let g:gruvbox_material_foreground = 'original'
+        let g:gruvbox_material_foreground = 'original'
 
-" For better performance
-let g:gruvbox_material_better_performance = 1
+        " For better performance
+        let g:gruvbox_material_better_performance = 1
 
-let g:gruvbox_material_ui_contrast = 'high'
+        let g:gruvbox_material_ui_contrast = 'high'
 
-let g:gruvbox_material_show_eob = 'false'
+        let g:gruvbox_material_show_eob = 'false'
 
-colorscheme gruvbox-material
+        colorscheme gruvbox-material
+        ]]
+    end
+}
 
-]]
+configs[colorscheme]()

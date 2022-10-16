e5d3dc9db582bc471d890627b35302780292039d
Doering Christian <christian.doering@tum.de>
2022-10-16 09:40:46 +0200

    client capabilities

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -157,13 +157,11 @@ M.on_attach = function(client, bufnr)
     vim.notify("LSP Client: " .. client.name)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
     return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M

79e6ff0f2fe0a0b6ca50c72c8da4fe52d1fb09f3
Döring Christian <christian.doering@um.de>
2022-10-17 09:33:14 +0200

    Disabled = spacing for conf files

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -96,7 +96,7 @@ npairs.add_rules {
         :use_key(']'),
     Rule('=', '')
         :with_pair(cond.not_inside_quote())
-        :with_pair(cond.not_filetypes({ "sh", "make", "desktop", "zsh" }))
+        :with_pair(cond.not_filetypes({ "sh", "make", "desktop", "zsh", "conf" }))
         :with_pair(function(opts)
             local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
             if last_char:match('[%w%=%s]') then

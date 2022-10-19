318cdbb45480c7c5247fcbb2f0af4c921c147ecb
Döring Christian <christian.doering@um.de>
2022-10-19 10:21:37 +0200

    Fast wrap on tab

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -13,9 +13,9 @@ local check_backspace = function()
     return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+local check_bracket = function()
+    local col = vim.fn.col '.' - 1
+    return vim.fn.getline('.'):sub(1, col):match "[%(%[%{\"']%s-$"
 end
 
 --   פּ ﯟ   some other good icons
@@ -102,6 +102,8 @@ cmp.setup {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif check_bracket() then
+                require('nvim-autopairs.fastwrap').show()
             elseif luasnip.expandable() then
                 luasnip.expand()
             elseif luasnip.expand_or_jumpable() then

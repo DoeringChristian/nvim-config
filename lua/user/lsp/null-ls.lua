06ab8cf85a91c30464cc589ec11c152d724ed1ed
Doering Christian <christian.doering@tum.de>
2022-11-04 19:32:14 +0100

    removed gitsigns

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,6 +11,6 @@ null_ls.setup {
         --null_ls.builtins.formatting.autopep8,
         --null_ls.builtins.code_actions.refactoring
         null_ls.builtins.formatting.black,
-        null_ls.builtins.code_actions.gitsigns
+        --null_ls.builtins.code_actions.gitsigns
     }
 }

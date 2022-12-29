421a817289495ff75b1b5df4f74828a66cc4306d
Doering Christian <christian.doering@tum.de>
2022-12-29 14:27:43 +0100

    fix better escape intervall

--- lua/user/escape.lua
+++ lua/user/escape.lua
@@ -1,5 +1,5 @@
 vim.g.better_escape_shortcut = { "jk", "jK", "JK", "Jk" }
-vim.g.better_escape_intervall = 1000
+vim.g.better_escape_interval = 1000
 
 local ok, escape = pcall(require, "better_escape")
 if not ok then

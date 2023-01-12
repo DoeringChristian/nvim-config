6448a9a81125b2a3dae3acb6e5e395c6343a50d4
Doering Christian <christian.doering@tum.de>
2023-01-12 13:47:48 +0100

    cmp sorting from tjdevries

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -156,20 +156,12 @@ cmp.setup {
     sources = cmp.config.sources(
         {
             { name = "nvim_lsp" },
-            {
-                name = "luasnip",
-                max_item_count = 4,
-            },
-            --{ name = "latex_symbols" },
-            --{ name = "cmp_pandoc" },
-            --{ name = "spell" },
+            { name = "luasnip" },
         },
         {
-            { name = "buffer" },
-            --{ name = "latex_symbols" },
-            --{ name = "cmp_pandoc" },
-            { name = "spell" },
             { name = "path" },
+            { name = "buffer", keyword_length = 5 },
+            { name = "spell" },
         }
     ),
     confirm_opts = {
@@ -183,11 +175,39 @@ cmp.setup {
         ghost_text = true,
         native_menu = false,
     },
-    sorting_ = {
+    --sorting_ = {
+    --    comparators = {
+    --        cmp.config.compare.score,
+    --        cmp.config.compare.offset,
+    --    }
+    --},
+    sorting = {
+        -- From https://github.com/tjdevries/config_manager/blob/40e9f32cff4f1a8f38fef3d3ea0d992417d6ef07/xdg_config/nvim/after/plugin/completion.lua
+        -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
         comparators = {
-            cmp.config.compare.score,
             cmp.config.compare.offset,
-        }
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            -- copied from cmp-under, but I don't think I need the plugin for this.
+            -- I might add some more of my own.
+            function(entry1, entry2)
+                local _, entry1_under = entry1.completion_item.label:find "^_+"
+                local _, entry2_under = entry2.completion_item.label:find "^_+"
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                    return false
+                elseif entry1_under < entry2_under then
+                    return true
+                end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
     },
     matching = {
         disallow_fuzzy_matching = false,

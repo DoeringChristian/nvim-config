0db863f2510b44d2a6bb2dc2ea34a84272c444d4
Doering Christian <christian.doering@tum.de>
2023-01-12 14:28:17 +0100

    : mapping

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -224,3 +224,17 @@ cmp.setup.cmdline('/', {
     }),
     mapping = cmp.mapping.preset.cmdline(),
 })
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!' }
+            }
+        }
+    })
+})

7f00e920f982cdab90a94ce13bf003d18501b49d
Döring Christian <christian.doering@um.de>
2023-02-18 22:58:55 +0100

    Hydra

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -87,5 +87,56 @@ return {
                 dap_hydra:activate()
             end
         end
+
+        local cmd = require('hydra.keymap-util').cmd
+
+        local hint = [[
+                 _f_: files       _m_: marks
+   🭇🬭🬭🬭🬭🬭🬭🬭🬭🬼    _o_: old files   _g_: live grep
+  🭉🭁🭠🭘    🭣🭕🭌🬾   _p_: projects    _/_: search in file
+  🭅█ ▁     █🭐   _z_: fzf
+  ██🬿      🭊██
+ 🭋█🬝🮄🮄🮄🮄🮄🮄🮄🮄🬆█🭀  _r_: resume      _u_: undotree
+ 🭤🭒🬺🬹🬱🬭🬭🬭🬭🬵🬹🬹🭝🭙  _h_: vim help    _c_: execute command
+                 _k_: keymaps     _;_: commands history
+                 _O_: options     _?_: search history
+ ^
+                 _<Enter>_: Telescope           _<Esc>_
+]]
+
+        Hydra({
+            name = 'Telescope',
+            hint = hint,
+            config = {
+                color = 'teal',
+                invoke_on_body = true,
+                hint = {
+                    position = 'middle',
+                    border = 'rounded',
+                },
+                desc = 'Hydra: [F]ind using Telescope'
+            },
+            mode = 'n',
+            body = '<leader>f',
+            heads = {
+                { 'f',       cmd 'Telescope find_files' },
+                { 'g',       cmd 'Telescope live_grep' },
+                { 'z',       cmd 'Telescope fzf' },
+                { 'o',       cmd 'Telescope oldfiles',                  { desc = 'recently opened files' } },
+                { 'h',       cmd 'Telescope help_tags',                 { desc = 'vim help' } },
+                { 'm',       cmd 'MarksListBuf',                        { desc = 'marks' } },
+                { 'k',       cmd 'Telescope keymaps' },
+                { 'O',       cmd 'Telescope vim_options' },
+                { 'r',       cmd 'Telescope resume' },
+                { 'p',       cmd 'Telescope projects',                  { desc = 'projects' } },
+                { '/',       cmd 'Telescope current_buffer_fuzzy_find', { desc = 'search in file' } },
+                { '?',       cmd 'Telescope search_history',            { desc = 'search history' } },
+                { ';',       cmd 'Telescope command_history',           { desc = 'command-line history' } },
+                { 'c',       cmd 'Telescope commands',                  { desc = 'execute command' } },
+                { 'u',       cmd 'silent! %foldopen! | UndotreeToggle', { desc = 'undotree' } },
+                { '<Enter>', cmd 'Telescope',                           { exit = true, desc = 'list all pickers' } },
+                { '<Esc>',   nil,                                       { exit = true, nowait = true } },
+            }
+        })
     end
 }

5ddce1734ae33f10502667d8294837e8cb9790fa
Doering Christian <christian.doering@tum.de>
2023-03-05 16:46:23 +0100

    updates and formatting

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -52,11 +52,11 @@ M.setup = function()
 
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
         vim.lsp.diagnostic.on_publish_diagnostics, {
-        -- Enable underline, use default values
-        underline = true,
-        -- Disable a feature
-        update_in_insert = false,
-    })
+            -- Enable underline, use default values
+            underline = true,
+            -- Disable a feature
+            update_in_insert = false,
+        })
 
     -- Configure Telescope for lsp handlers
 

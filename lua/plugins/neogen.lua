ae94ba6cc5652a6dacec41d2d927b7a9de0451d8
Döring Christian <christian.doering@um.de>
2023-06-17 15:53:56 +0200

    cleanup and update

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -2,6 +2,13 @@ return {
     "danymat/neogen",
     dependencies = "nvim-treesitter/nvim-treesitter",
     config = true,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*"
+    keys = {
+        {
+            "<leader>ng", function() require "neogen".generate() end, desc = "[N]eogen [G]enerate"
+        },
+    },
+    config = function()
+        require "neogen".setup { snippet_engine = "luasnip" }
+    end
+
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,14 +2,13 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-hop.nvim",
         "tom-anders/telescope-vim-bookmarks.nvim",
         -- "nvim-telescope/telescope-ui-select.nvim",
         "gbprod/yanky.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     keys = {
-        { "<leader><Tab>", "<cmd>Telescope live_grep<cr>", desc = "Live Greph" },
+        -- { "<leader><Tab>", "<cmd>Telescope live_grep<cr>", desc = "Live Greph" },
     },
     config = function()
         local telescope = require "telescope"
@@ -36,7 +35,7 @@ return {
                     i = {
                         ["<C-n>"] = actions.cycle_history_next,
                         ["<C-p>"] = actions.cycle_history_prev,
-                        ["<C-h>"] = R("telescope").extensions.hop.hop,
+                        -- ["<C-h>"] = R("telescope").extensions.hop.hop,
                         ["<C-j>"] = actions.move_selection_next,
                         ["<C-k>"] = actions.move_selection_previous,
                         ["<C-c>"] = actions.close,
@@ -84,8 +83,8 @@ return {
                         ["<C-d>"] = actions.preview_scrolling_down,
                         ["<PageUp>"] = actions.results_scrolling_up,
                         ["<PageDown>"] = actions.results_scrolling_down,
-                        ["s"] = R("telescope").extensions.hop.hop,
-                        ["S"] = R("telescope").extensions.hop.hop,
+                        -- ["s"] = R("telescope").extensions.hop.hop,
+                        -- ["S"] = R("telescope").extensions.hop.hop,
                         ["?"] = actions.which_key,
                     },
                 },
@@ -131,35 +130,11 @@ return {
                 },
             },
             extensions = {
-                -- Your extension configuration goes here:
-                -- extension_name = {
-                --   extension_config_key = value,
-                -- }
-                -- please take a look at the readme of the extension you want to configure
-                hop = {
-                    keys = { "w", "e", "r", "t",
-                        "z", "u", "i", "o",
-                        "p", "g", "h", "y",
-                        "x", "c", "v", "b",
-                        "n", "m", "a", "l",
-                        "s", "k", "d", "j", "f", },
-                    sign_hl = { "WarningMsg", "Title" },
-                },
-                -- ['ui-select'] = {
-                --     initial_mode = "normal"
-                -- },
-                -- fzf = {
-                --     fuzzy = true,                   -- false will only do exact matching
-                --     override_generic_sorter = true, -- override the generic sorter
-                --     override_file_sorter = true,    -- override the file sorter
-                --     case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-                --     -- the default case_mode is "smart_case"
-                -- },
             },
         }
-        -- require "telescope".load_extension("fzf")
+        require "telescope".load_extension("fzf")
         require "telescope".load_extension("yank_history")
-        require "telescope".load_extension("hop")
-        require "telescope".load_extension("vim_bookmarks")
+        -- require "telescope".load_extension("hop")
+        -- require "telescope".load_extension("vim_bookmarks")
     end
 }

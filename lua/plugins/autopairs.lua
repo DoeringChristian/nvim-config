88145c7d3f26f271c09520ce10bdf3b497d7036f
Döring Christian <christian.doering@um.de>
2023-06-20 16:59:26 +0200

    formatting

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -37,30 +37,30 @@ return {
         npairs.add_rules({
             Rule("$", "$", { "tex", "latex", "markdown" })
             -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex("%%"))
-            :with_move(function(opts)
-                return opts.prev_char:match('%$') ~= nil
-            end)
+                :with_pair(cond.not_after_regex("%%"))
+                :with_move(function(opts)
+                    return opts.prev_char:match('%$') ~= nil
+                end)
             -- don't move right when repeat character
             --:with_move(cond.none())
             -- disable adding a newline when you press <cr>
-            :with_cr(cond.none())
+                :with_cr(cond.none())
         })
 
         -- $|$ (<Space>)-> $ | $ in tex, latex, markdown
         -- $ | $ ($)-> $  $|
         npairs.add_rules({
             Rule(" ", " ", { "tex", "latex", "markdown" })
-            :with_pair(function(opts)
-                local pair = opts.line:sub(opts.col - 1, opts.col)
-                return vim.tbl_contains({ '$$' }, pair)
-            end),
+                :with_pair(function(opts)
+                    local pair = opts.line:sub(opts.col - 1, opts.col)
+                    return vim.tbl_contains({ '$$' }, pair)
+                end),
             Rule('$ ', ' $')
             --:with_pair(function() return false end)
-            :with_move(function(opts)
-                return opts.prev_char:match('%$') ~= nil
-            end)
-            :use_key('$'),
+                :with_move(function(opts)
+                    return opts.prev_char:match('%$') ~= nil
+                end)
+                :use_key('$'),
         })
         --[[
 npairs.add_rules({
@@ -77,56 +77,56 @@ npairs.add_rules({
         -- {|} -> { | }
         npairs.add_rules {
             Rule(' ', ' ')
-            :with_pair(function(opts)
-                local pair = opts.line:sub(opts.col - 1, opts.col)
-                return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-            end),
+                :with_pair(function(opts)
+                    local pair = opts.line:sub(opts.col - 1, opts.col)
+                    return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+                end),
             Rule('( ', ' )')
-            :with_pair(function() return false end)
-            :with_move(function(opts)
-                return opts.prev_char:match('.%)') ~= nil
-            end)
-            :use_key(')'),
+                :with_pair(function() return false end)
+                :with_move(function(opts)
+                    return opts.prev_char:match('.%)') ~= nil
+                end)
+                :use_key(')'),
             Rule('{ ', ' }')
-            :with_pair(function() return false end)
-            :with_move(function(opts)
-                return opts.prev_char:match('.%}') ~= nil
-            end)
-            :use_key('}'),
+                :with_pair(function() return false end)
+                :with_move(function(opts)
+                    return opts.prev_char:match('.%}') ~= nil
+                end)
+                :use_key('}'),
             Rule('[ ', ' ]')
-            :with_pair(function() return false end)
-            :with_move(function(opts)
-                return opts.prev_char:match('.%]') ~= nil
-            end)
-            :use_key(']'),
+                :with_pair(function() return false end)
+                :with_move(function(opts)
+                    return opts.prev_char:match('.%]') ~= nil
+                end)
+                :use_key(']'),
             Rule('=', '')
-            :with_pair(cond.not_inside_quote())
-            :with_pair(cond.not_filetypes({ "sh", "make", "desktop", "zsh", "conf" }))
-            :with_pair(function(opts)
-                local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
-                if last_char:match('[%w%=%s]') then
-                    return true
-                end
-                return false
-            end)
-            :replace_endpair(function(opts)
-                local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
-                local next_char = opts.line:sub(opts.col, opts.col)
-                next_char = next_char == ' ' and '' or ' '
-                if prev_2char:match('%w$') then
-                    return '<bs> =' .. next_char
-                end
-                if prev_2char:match('%=$') then
-                    return next_char
-                end
-                if prev_2char:match('=') then
-                    return '<bs><bs>=' .. next_char
-                end
-                return ''
-            end)
-            :set_end_pair_length(0)
-            :with_move(cond.none())
-            :with_del(cond.none())
+                :with_pair(cond.not_inside_quote())
+                :with_pair(cond.not_filetypes({ "sh", "make", "desktop", "zsh", "conf" }))
+                :with_pair(function(opts)
+                    local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+                    if last_char:match('[%w%=%s]') then
+                        return true
+                    end
+                    return false
+                end)
+                :replace_endpair(function(opts)
+                    local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+                    local next_char = opts.line:sub(opts.col, opts.col)
+                    next_char = next_char == ' ' and '' or ' '
+                    if prev_2char:match('%w$') then
+                        return '<bs> =' .. next_char
+                    end
+                    if prev_2char:match('%=$') then
+                        return next_char
+                    end
+                    if prev_2char:match('=') then
+                        return '<bs><bs>=' .. next_char
+                    end
+                    return ''
+                end)
+                :set_end_pair_length(0)
+                :with_move(cond.none())
+                :with_del(cond.none())
         }
 
 

b75e0f17dcab08a693c1ce9f62a141c481dcce6e
Doering Christian <christian.doering@tum.de>
2023-07-03 18:49:25 +0200

    UpdateLspSettings function

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -55,5 +55,31 @@ M.imap = function(keys, func, desc)
     M.map('i', keys, func, desc)
 end
 
+---@param server_name string
+---@param settings_patcher fun(settings: table): table
+function UpdateLspSettings(server_name, settings_patcher)
+    local function patch_settings(client)
+        settings_patcher(client.config.settings)
+        client.notify("workspace/didChangeConfiguration", {
+            settings = client.config.settings,
+        })
+    end
+
+    local clients = vim.lsp.get_active_clients({ name = server_name })
+    if #clients > 0 then
+        patch_settings(clients[1])
+        return
+    end
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client.name == server_name then
+                patch_settings(client)
+                return true
+            end
+        end,
+    })
+end
 
 return M

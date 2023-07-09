2ab5100bd4637f66a874b972f9f3a31754ad3490
Doering Christian <christian.doering@tum.de>
2023-07-09 22:12:52 +0200

    nabla

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -4,11 +4,24 @@ return {
         vim.api.nvim_create_autocmd({ "Filetype" }, {
             pattern = { "markdown", "tex" },
             callback = function()
+                local function map(mode, keys, func, desc)
+                    if desc then
+                        desc = 'Nabla: ' .. desc
+                    end
+                    vim.keymap.set(mode, keys, func, { noremap = true, silent = true, buffer = bufnr, desc = desc })
+                end
+
                 -- Enable nabla for markdown
                 require "nabla".enable_virt({
                     autogen = true, -- auto-regenerate ASCII art when exiting insert mode
-                    silent = true,  -- silents error messages
+                    silent = false, -- silents error messages
                 })
+                map("n", "<leader>p", function()
+                    return require "nabla".popup()
+                end, "[P]opup")
+                map("n", "<leader>nv", function()
+                    return require "nabla".toggle_virt()
+                end, "[N]abla [V]irt")
             end
         })
     end

d9844477c93cb6a2d32ec40a279d5a9d2eed8ce1
Doering Christian <christian.doering@tum.de>
2023-07-14 11:25:51 +0200

    inc-rename expand word

--- lua/plugins/inc-rename.lua
+++ lua/plugins/inc-rename.lua
@@ -1,7 +1,13 @@
 return {
     "smjonas/inc-rename.nvim",
     keys = {
-        { "<leader>rn", ":IncRename " }
+        {
+            "<leader>rn",
+            function()
+                return ":IncRename " .. vim.fn.expand("<cword>")
+            end,
+            expr = true
+        }
     },
     opts = {
         preview_empty_name = true,

67fe7a4e03f7f577da28d0f13c5abc93ea801aea
Doering Christian <christian.doering@tum.de>
2023-07-15 10:38:23 +0200

    added neogit back in

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim"
+    },
+    opts = {
+        integrations = {
+            diffview = true,
+        },
+    }
+}

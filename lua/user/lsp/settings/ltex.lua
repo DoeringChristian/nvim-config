bea823df134b9b52c38faf76d99f287d7915d285
Doering Christian <christian.doering@tum.de>
2023-07-15 13:26:59 +0200

    Removed completion for ltex because I suspect it to cause some of the lag.

--- lua/user/lsp/settings/ltex.lua
+++ lua/user/lsp/settings/ltex.lua
@@ -1,11 +1,11 @@
 return {
-    filetypes = { "markdown", "md", "tex", "python" },
+    filetypes = { "markdown", "md", "tex", "python", "rust" },
     flags = { debounce_text_changes = 500 },
     settings = {
         ltex = {
             enabled = { "bibtex", "context", "context.tex", "html", "latex", "markdown", "org", "restructuredtext",
-                "rsweave", "python" },
-            completionEnabled = true,
+                "rsweave", "python", "rust" },
+            completionEnabled = false,
             markdown = {
                 nodes = {
                     FencedCodeBlock = "ignore",

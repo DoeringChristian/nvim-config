f88560c6b56564799f0cd2a7a4c2d10de6273534
Doering Christian <christian.doering@tum.de>
2023-07-15 13:02:33 +0200

    disable ltex for rust

--- lua/user/lsp/settings/ltex.lua
+++ lua/user/lsp/settings/ltex.lua
@@ -1,10 +1,10 @@
 return {
-    filetypes = { "markdown", "md", "tex", "python", "rust" },
+    filetypes = { "markdown", "md", "tex", "python" },
     flags = { debounce_text_changes = 500 },
     settings = {
         ltex = {
             enabled = { "bibtex", "context", "context.tex", "html", "latex", "markdown", "org", "restructuredtext",
-                "rsweave", "python", "rust" },
+                "rsweave", "python" },
             completionEnabled = true,
             markdown = {
                 nodes = {

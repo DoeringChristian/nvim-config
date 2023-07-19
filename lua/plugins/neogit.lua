3dabd14139f703b77918883fd8beeb41aed4ba18
Döring Christian <christian.doering@um.de>
2023-07-19 12:14:10 +0200

    try set neogit highlitht

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -10,6 +10,23 @@ return {
         },
     },
     config = function(_, opts)
+        local function hi(name, opts)
+            vim.api.nvim_set_hl(0, name, opts)
+        end
+
+
         require "neogit".setup(opts)
+
+        hi('NeogitCommitViewHeader', { link = 'Special' })
+        hi('NeogitDiffAddHighlight', { link = 'DiffAdd' })
+        hi('NeogitDiffAdd', { link = 'DiffAdd' })
+        hi('NeogitDiffDeleteHighlight', { link = 'DiffDelete' })
+        hi('NeogitDiffDelete', { link = 'DiffDelete' })
+        hi('NeogitFold', { link = 'FoldColumn' })
+        -- hi('NeogitHunkHeader', { fg = p.base0D, bg = nil, attr = nil, sp = nil })
+        -- hi('NeogitHunkHeaderHighlight', { fg = p.base0D, bg = nil, attr = 'bold', sp = nil })
+        hi('NeogitNotificationError', { link = 'DiagnosticError' })
+        hi('NeogitNotificationInfo', { link = 'DiagnosticInfo' })
+        hi('NeogitNotificationWarning', { link = 'DiagnosticWarn' })
     end,
 }

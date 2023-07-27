3f096a1bdbc175d4bac59a994ebd3d4a336ded75
Döring Christian <christian.doering@um.de>
2023-07-27 14:28:23 +0200

    disable ltex for python

--- lua/user/lsp/settings/ltex.lua
+++ lua/user/lsp/settings/ltex.lua
@@ -1,10 +1,10 @@
 return {
-    filetypes = { "markdown", "md", "tex", "python" },
+    filetypes = { "markdown", "md", "tex" },
     flags = { debounce_text_changes = 500 },
     settings = {
         ltex = {
             enabled = { "bibtex", "context", "context.tex", "html", "latex", "markdown", "org", "restructuredtext",
-                "rsweave", "python" },
+                "rsweave" },
             completionEnabled = true,
             markdown = {
                 nodes = {

f8231dcca0a5e43fdcb33ecbd7b264c477bc26a9
Christian Döring <christian.doering@tum.de>
2023-09-10 11:24:39 +0200

    "Some fixes for slang"

--- lua/user/lsp/settings/slang.lua
+++ lua/user/lsp/settings/slang.lua
@@ -1,7 +1,9 @@
 return {
     settings = {
+        enableCommitCharactersInAutoCompletion = false,
         slang = {
             searchInAllWorkspaceDirectories = true,
+            enableCommitCharactersInAutoCompletion = false,
             -- additionalSearchPaths = { "/home/doeringc/workspace/cpp/Falcor/Source/Falcor/" },
         }
     }

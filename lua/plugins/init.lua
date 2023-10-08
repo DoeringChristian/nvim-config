681da1a4eed862c9fb74c8dfb665227682793c20
Christian Döring <christian.doering@tum.de>
2023-10-08 10:48:31 +0200

    Update and fixes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,13 +10,19 @@ return {
     {
         "klen/nvim-config-local",
         config = function()
-            require "config-local".setup {}
-        end
+            require("config-local").setup({})
+        end,
     },
     {
         "windwp/nvim-spectre",
         keys = {
-            { "<leader>sr", function() require("spectre").open() end, desc = "[S]earch [R]eplace" },
+            {
+                "<leader>sr",
+                function()
+                    require("spectre").open()
+                end,
+                desc = "[S]earch [R]eplace",
+            },
         },
     },
 }
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,24 +3,23 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local map = require "util".map
-local xmap = require "util".xmap
-local vmap = require "util".vmap
-local nmap = require "util".nmap
-local imap = require "util".imap
-
+local map = require("util").map
+local xmap = require("util").xmap
+local vmap = require("util").vmap
+local nmap = require("util").nmap
+local imap = require("util").imap
 
 -- Resize with arrows
-nmap('<C-Up>', ':resize +2<CR>', "Increase vertical size of current Buffer")
-nmap('<C-Down>', ':resize -2<CR>', "Decrease vertical size of current Buffer")
-nmap('<C-Right>', ':vertical resize +2<CR>', "Increase horizontal size of current Buffer")
-nmap('<C-Left>', ':vertical resize -2<CR>', "Decrease horizontal size of current Buffer")
+nmap("<C-Up>", ":resize +2<CR>", "Increase vertical size of current Buffer")
+nmap("<C-Down>", ":resize -2<CR>", "Decrease vertical size of current Buffer")
+nmap("<C-Right>", ":vertical resize +2<CR>", "Increase horizontal size of current Buffer")
+nmap("<C-Left>", ":vertical resize -2<CR>", "Decrease horizontal size of current Buffer")
 
 -- Navigate buffers
-nmap('<S-l>', ":bnext<CR>", "Go one Buffer left")
-nmap('<S-h>', ":bprevious<CR>", "Go one Buffer right")
-nmap('<leader>bc', "<cmd>BufferLinePickClose<CR>", "[B]uffer [C]lose")
-nmap('<leader>sb', "<cmd>BufferLinePick<CR>", "[S]elect [B]uffer")
+nmap("<S-l>", ":bnext<CR>", "Go one Buffer left")
+nmap("<S-h>", ":bprevious<CR>", "Go one Buffer right")
+nmap("<leader>bc", "<cmd>BufferLinePickClose<CR>", "[B]uffer [C]lose")
+nmap("<leader>sb", "<cmd>BufferLinePick<CR>", "[S]elect [B]uffer")
 
 -- Visual --
 -- Stay in indent mode
@@ -28,8 +27,8 @@ vmap("<", "<gv")
 vmap(">", ">gv")
 
 -- Move text up and down
-vmap('<A-j>', ':m .+1<CR>', "Move Line Down")
-vmap('<A-k>', ':m .-2<CR>', "Move Line Up")
+vmap("<A-j>", ":m .+1<CR>", "Move Line Down")
+vmap("<A-k>", ":m .-2<CR>", "Move Line Up")
 --vmap("p", '"_dP')
 
 -- Visual Block --
@@ -38,12 +37,14 @@ map("x", "<A-j>", ":move '>+1<CR>gv-gv", "Move Line Down")
 map("x", "<A-k>", ":move '<-2<CR>gv-gv", "Move Line Up")
 
 -- Telescope --
-nmap("<leader><Tab>",
-    function()
-        return require 'telescope.builtin'.grep_string { shorten_path = true, word_match =
-        "-w", only_sort_text = true, search = '' }
-    end,
-    "Live Grep")
+nmap("<leader><Tab>", function()
+    return require("telescope.builtin").grep_string({
+        shorten_path = true,
+        word_match = "-w",
+        only_sort_text = true,
+        search = "",
+    })
+end, "Live Grep")
 
 -- Surround --
 -- Need to remap surround.vim mappings to not conflict with leap.nvmi
@@ -62,7 +63,7 @@ xmap("gs", "<Plug>VSurround", "Surround Visual")
 xmap("gS", "<Plug>VgSurround", "Surround Visual with Newline")
 
 -- GUI font size --
-vim.cmd [[
+vim.cmd([[
 nnoremap <C-+> :silent! let &guifont = substitute(
  \ &guifont,
  \ ':h\zs\d\+',
@@ -73,10 +74,10 @@ nnoremap <C--> :silent! let &guifont = substitute(
  \ ':h\zs\d\+',
  \ '\=eval(submatch(0)-1)',
  \ '')<CR>
-]]
+]])
 
 -- LuaSnippet --
-nmap('<leader>lse', '<cmd>LuaSnipEdit<CR>', "[L]ua [S]nip [E]dit")
+nmap("<leader>lse", "<cmd>LuaSnipEdit<CR>", "[L]ua [S]nip [E]dit")
 
 -- Leap search for operator pending mode (remove if causes issues)
 map("o", "s", "<Plug>(leap-forward)")
@@ -94,10 +95,10 @@ map("o", "S", "<Plug>(leap-backward)")
 -- nmap("<leader>rp", "vip<cmd>SnipRun<cr><esc>", "[R]un [P]aragraph")
 -- vmap("<leader>rr", "<cmd>SnipRun<cr>", "[R]un Selection")
 -- map("o", "<leader>rr", "<Plug>SnipRunOperator", "[R]un for")
-nmap('<space>rs', '<cmd>IronRepl<cr>')
-nmap('<space>rr', '<cmd>IronRestart<cr>')
-nmap('<space>rf', '<cmd>IronFocus<cr>')
-nmap('<space>rh', '<cmd>IronHide<cr>')
+nmap("<space>rs", "<cmd>IronRepl<cr>")
+nmap("<space>rr", "<cmd>IronRestart<cr>")
+nmap("<space>rf", "<cmd>IronFocus<cr>")
+nmap("<space>rh", "<cmd>IronHide<cr>")
 
 -- Gitsigns mappings
 nmap("]c", "<cmd>Gitsigns next_hunk<cr>", "Next [G]it Hunk")

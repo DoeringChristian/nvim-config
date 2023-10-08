16da3e91a9747276e18642add83d794dfa7db107
Christian Döring <christian.doering@tum.de>
2023-10-08 17:02:48 +0200

    Fixed telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -46,54 +46,57 @@ return {
             desc = '[F]ind [D]iagnostics',
         },
     },
-    opts = {
-        defaults = {
-            mappings = {
-                i = {
-                    ['<C-u>'] = false,
-                    ['<C-d>'] = false,
+    config = function()
+        local actions = require "telescope.actions"
+        require "telescope".setup {
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                    },
                 },
             },
-        },
-        pickers = {
-            -- Default configuration for builtin pickers goes here:
-            -- picker_name = {
-            --   picker_config_key = value,
-            --   ...
-            -- }
-            -- Now the picker_config_key will be applied every time you call this
-            -- builtin picker
-            lsp_references = {
-                initial_mode = 'normal',
+            pickers = {
+                -- Default configuration for builtin pickers goes here:
+                -- picker_name = {
+                --   picker_config_key = value,
+                --   ...
+                -- }
+                -- Now the picker_config_key will be applied every time you call this
+                -- builtin picker
+                lsp_references = {
+                    initial_mode = 'normal',
+                },
+                lsp_definitions = {
+                    initial_mode = 'normal',
+                },
+                lsp_implementations = {
+                    initial_mode = 'normal',
+                },
+                lsp_document_symbols = {
+                    --initial_mode = "normal",
+                },
+                lsp_incoming_calls = {
+                    initial_mode = 'normal',
+                },
+                lsp_outgoing_calls = {
+                    initial_mode = 'normal',
+                },
+                diagnostics = {
+                    initial_mode = 'normal',
+                },
+                lsp_type_definitions = {
+                    initial_mode = 'normal',
+                },
+                topts = {
+                    initial_mode = 'normal',
+                },
+                colorscheme = {
+                    initial_mode = 'normal',
+                    enable_preview = true,
+                },
             },
-            lsp_definitions = {
-                initial_mode = 'normal',
-            },
-            lsp_implementations = {
-                initial_mode = 'normal',
-            },
-            lsp_document_symbols = {
-                --initial_mode = "normal",
-            },
-            lsp_incoming_calls = {
-                initial_mode = 'normal',
-            },
-            lsp_outgoing_calls = {
-                initial_mode = 'normal',
-            },
-            diagnostics = {
-                initial_mode = 'normal',
-            },
-            lsp_type_definitions = {
-                initial_mode = 'normal',
-            },
-            topts = {
-                initial_mode = 'normal',
-            },
-            colorscheme = {
-                initial_mode = 'normal',
-                enable_preview = true,
-            },
-        },
-    },
+        }
+    end
 }

b1c587b0713a812bbeea9025fbf2f0f90b576354
Christian Döring <christian.doering@tum.de>
2024-06-02 12:31:13 +0200

    obsidian gr mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -105,3 +105,8 @@ nmap("<C-h>", "<C-w>h", "")
 nmap("<C-j>", "<C-w>j", "")
 nmap("<C-k>", "<C-w>k", "")
 nmap("<C-l>", "<C-w>l", "")
+
+-- Delete default gr mappings
+vim.keymap.del("n", "grn")
+vim.keymap.del("n", "gra")
+vim.keymap.del("n", "grr")
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -89,6 +89,12 @@ return {
                 end,
                 opts = { buffer = true, expr = true },
             },
+            ["gr"] = {
+                action = function()
+                    return [[<cmd>ObsidianBacklinks<CR>]]
+                end,
+                opts = { noremap = false, expr = true, buffer = true },
+            },
 
         },
         new_notes_location = "current_dir",
@@ -129,6 +135,27 @@ return {
             -- This is equivalent to the default behavior.
             local path = spec.dir / tostring(spec.id)
             return path:with_suffix(".md")
+        end,
+
+        -- Optional, alternatively you can customize the frontmatter data.
+        ---@return table
+        note_frontmatter_func = function(note)
+            -- Add the title of the note as an alias.
+            if note.title then
+                note:add_alias(note.title)
+            end
+
+            local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+            -- `note.metadata` contains any manually added fields in the frontmatter.
+            -- So here we just make sure those fields are kept in the frontmatter.
+            if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                for k, v in pairs(note.metadata) do
+                    out[k] = v
+                end
+            end
+
+            return out
         end,
 
         picker = {

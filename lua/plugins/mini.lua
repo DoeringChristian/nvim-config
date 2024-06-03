0e89f7f065559bc4abad24c972ee5ea66ac94fd6
Christian Döring <christian.doering@tum.de>
2024-06-03 19:39:34 +0200

    Mini.ai increase line size

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -20,7 +20,9 @@ return {
         "echasnovski/mini.ai",
         version = false,
         config = function()
-            require "mini.ai".setup {}
+            require "mini.ai".setup {
+                n_lines = 1000,
+            }
         end
     },
 }

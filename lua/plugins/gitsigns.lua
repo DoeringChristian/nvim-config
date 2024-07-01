adf296267331ad57c7f3f1f6af26783a21e3c7be
Christian Döring <christian.doering@tum.de>
2024-07-01 22:12:15 +0200

    Update + gitsigns yadm fix

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,20 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "seanbreckenridge/gitsigns-yadm.nvim",
+            opts = {
+                shell_timeout_ms = 1000,
+            },
+        },
+    },
     opts = {
         sign_priority = 1,
+        _on_attach_pre = function(_, callback)
+            require "gitsigns-yadm".yadm_signs(callback)
+        end,
         on_attach = function(bufnr)
             local gs = package.loaded.gitsigns
 

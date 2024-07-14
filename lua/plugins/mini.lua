30d68f2da1e7bbb50bf41a89e4bb11d008ecb894
Christian Döring <christian.doering@tum.de>
2024-07-14 22:29:41 +0200

    Better treesitter mini.ai mappings

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -18,10 +18,26 @@ return {
     },
     {
         "echasnovski/mini.ai",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            { "nvim-treesitter/nvim-treesitter-textobjects", },
+        },
         version = false,
+        enabled = true,
         config = function()
+            local gen_spec = require "mini.ai".gen_spec
             require "mini.ai".setup {
                 n_lines = 1000,
+                custom_textobjects = {
+                    F = gen_spec.treesitter {
+                        a = { "@function.outer" },
+                        i = { "@function.inner" }
+                    },
+                    o = gen_spec.treesitter {
+                        a = { "@function.outer", "@class.outer", "@loop.outer", "@conditional.outer", "@comment.outer" },
+                        i = { "@function.inner", "@class.inner", "@loop.inner", "@conditional.inner", "@comment.inner" }
+                    },
+                }
             }
         end
     },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        dependencies = {
+            { "nvim-treesitter/nvim-treesitter-textobjects", },
+        },
         config = function()
             local treesitter = require("nvim-treesitter.configs")
             treesitter.setup({
@@ -41,6 +44,33 @@ return {
 
                 highlight = { enable = true },
                 indent = { enable = true },
+
+                textobjects = {
+                    lsp_interop = {
+                        enable = false,
+                        border = 'none',
+                        floating_preview_opts = {},
+
+                    },
+                    select = {
+                        enable = false,
+                        lookahead = true,
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = { query = "@function.outer", desc = "Select [A]rround [F]unction" },
+                            ["if"] = { query = "@function.inner", desc = "Select [I]nside [F]unction" },
+                            ["ac"] = { query = "@class.outer", desc = "Select [A]rround [C]lass" },
+                            ["ic"] = { query = "@class.inner", desc = "Select [I]nside [C]lass" },
+                            ["ag"] = { query = "@comment.outer", desc = "Select [A]rround Comment" },
+                            ["ig"] = { query = "@comment.inner", desc = "Select [I]nside Comment" },
+                            ["al"] = { query = "@loop.outer", desc = "Select [A]rround [L]oop" },
+                            ["il"] = { query = "@loop.inner", desc = "Select [I]nside [L]oop" },
+                            -- ["ab"] = { query = "@block.outer", desc = "Select [A]rround [B]lock" },
+                            -- ["ib"] = { query = "@bloc.inner", desc = "Select [I]nside [B]lock" },
+                        },
+                    }
+
+                },
             })
         end,
     },

89611a2202240896efb4b51a27ec44b1186d79f6
Christian Döring <christian.doering@tum.de>
2024-07-15 08:09:25 +0200

    Fixed colors ksb

--- ksb.lua
+++ ksb.lua
@@ -1,16 +1,5 @@
 -- Minimal config for kitty-scrollback.nvim
-
--- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
--- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
-vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/kitty-scrollback.nvim") -- lazy.nvim
-require "kitty-scrollback".setup {
-    -- put your kitty-scrollback.nvim configurations here
-}
-
--- Load catppuccin
-vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/catppuccin") -- lazy.nvim
-vim.cmd.colorscheme "catppuccin"
-
+--
 -- basic mappings
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -19,3 +8,10 @@ vim.opt.termguicolors = true
 vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/better-escape.vim") -- lazy.nvim
 vim.g.better_escape_shortcut = { "jk", "jK", "JK", "Jk" }
 vim.g.better_escape_interval = 1000
+
+-- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
+-- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
+vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/kitty-scrollback.nvim") -- lazy.nvim
+require "kitty-scrollback".setup {
+    -- put your kitty-scrollback.nvim configurations here
+}

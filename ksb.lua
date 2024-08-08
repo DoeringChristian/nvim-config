26ae485ada20711b1a8c3bda0c73463a13024182
Christian Döring <christian.doering@tum.de>
2024-08-08 18:58:12 +0200

    Fixed close after yank to system clipboard

--- ksb.lua
+++ ksb.lua
@@ -9,24 +9,6 @@ vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/better-escape.vim") -
 vim.g.better_escape_shortcut = { "jk", "jK", "JK", "Jk" }
 vim.g.better_escape_interval = 1000
 
-vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/yanky.nvim") -- lazy.nvim
-
-local yanky = require "yanky"
-
-yanky.setup({
-    ring = {
-        history_length = 10,
-        storage = "shada",
-        sync_with_numbered_registers = true,
-    },
-    system_clipboard = {
-        sync_with_ring = true,
-    },
-    preserver_cursor_position = {
-        enabled = false,
-    },
-})
-
 -- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
 -- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
 vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/kitty-scrollback.nvim") -- lazy.nvim
@@ -35,11 +17,17 @@ ksb.setup {
     -- put your kitty-scrollback.nvim configurations here
     function()
         local ksb_api = require "kitty-scrollback.api"
-        vim.keymap.set({ 'v' }, 'Y', '<Plug>(KsbVisualYankLine)', {})
-        vim.keymap.set({ 'v' }, 'y', '<Plug>(KsbVisualYank)', {})
-        vim.keymap.set({ 'n' }, 'Y', '<Plug>(KsbNormalYankEnd)', {})
-        vim.keymap.set({ 'n' }, 'y', '<Plug>(KsbNormalYank)', {})
-        vim.keymap.set({ 'n' }, 'yy', '<Plug>(KsbYankLine)', {})
+
+
+        -- Yank mappings
+        vim.keymap.set({ "n" }, "Y", '"+Y', { noremap = true, silent = true, desc = "" })
+        vim.keymap.set({ "v" }, "Y", '"+Y', { noremap = true, silent = true, desc = "" })
+
+        vim.keymap.set({ "n" }, "yy", '"+yy', { noremap = true, silent = true, desc = "" })
+
+        vim.keymap.set({ "n" }, "y", '"+y', { noremap = true, silent = true, desc = "" })
+        vim.keymap.set({ "v" }, "y", '"+y', { noremap = true, silent = true, desc = "" })
+
         vim.keymap.set("n", "q", function() ksb_api.close_or_quit_all() end,
             { noremap = true, silent = true, desc = "[Q]uit" })
         vim.keymap.set("n", "i", function() ksb_api.close_or_quit_all() end,
@@ -50,8 +38,16 @@ ksb.setup {
         vim.keymap.set({ "n" }, "<C-j>", "<C-w>j", { noremap = true, silent = true, desc = "" })
         vim.keymap.set({ "n" }, "<C-k>", "<C-w>k", { noremap = true, silent = true, desc = "" })
         vim.keymap.set({ "n" }, "<C-l>", "<C-w>l", { noremap = true, silent = true, desc = "" })
+
         return {
-            paste_window = { yank_register = 'a' },
+            callbacks = {
+                after_ready = function()
+                    -- Disable Yank post autocmd
+                    -- https://github.com/mikesmithgh/kitty-scrollback.nvim/blob/dc101d0a8356db9c7290dfbfa82c27ec35e3b55a/lua/kitty-scrollback/autocommands.lua#L138
+                    vim.api.nvim_del_augroup_by_name("KittyScrollBackNvimTextYankPost")
+                end
+            },
+            keymaps_enabled = false,
         }
     end
 }

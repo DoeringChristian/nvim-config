3377e65a8f09d7ab0959ec5d9fa76f5f52330f35
Christian Döring <christian.doering@tum.de>
2024-08-09 14:30:11 +0200

    Leap in ksb

--- ksb.lua
+++ ksb.lua
@@ -9,6 +9,57 @@ vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/better-escape.vim") -
 vim.g.better_escape_shortcut = { "jk", "jK", "JK", "Jk" }
 vim.g.better_escape_interval = 1000
 
+-- Setup leap.nvim
+vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/leap.nvim")
+
+local leap = require "leap"
+leap.set_default_keymaps()
+leap.setup({
+    max_phase_one_targets = nil,
+    highlight_unlabeled_phase_one_targets = false,
+    max_highlighted_traversal_targets = 10,
+    case_sensitive = false,
+    equivalence_classes = { " \t\r\n" },
+    substitute_chars = {},
+    relative_directions = true,
+    labels = {
+        "s",
+        "f",
+        "n",
+        "u",
+        "t",
+        "r",
+        "j",
+        "k",
+        "l",
+        "o",
+        "d",
+        "w",
+        "e",
+        "h",
+        "m",
+        "v",
+        "g",
+        "c",
+        ".",
+        "z",
+    },
+    safe_labels = { "s", "f", "n", "u", "t", "r" },
+    --"F", "L", "N", "H", "G", "M", "U", "T", "Z" },
+    special_keys = {
+        repeat_search = "<enter>",
+        next_phase_one_target = "<enter>",
+        next_target = { "<enter>", ";" },
+        prev_target = { "<tab>", "," },
+        next_group = "<space>",
+        prev_group = "<tab>",
+        multi_accept = "<enter>",
+        multi_revert = "<backspace>",
+    },
+})
+
+vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+
 -- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
 -- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
 vim.opt.runtimepath:append(vim.fn.stdpath "data" .. "/lazy/kitty-scrollback.nvim") -- lazy.nvim

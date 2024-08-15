402b2eaa55e791edfba1c9993eba25e797525d22
Christian Döring <christian.doering@tum.de>
2024-08-15 20:10:55 +0200

    Update and git blame current line

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -11,7 +11,12 @@ return {
         },
     },
     opts = {
-        sign_priority = 1,
+        -- signs_staged_enable = false,
+        numhl = true,
+        signcolumn = true,
+        -- sign_priority = 1,
+        current_line_blame = true,
+        current_line_blame_formatter = "<author>, <author_time:%R> - <abbrev_sha> <summary>",
         _on_attach_pre = function(_, callback)
             require "gitsigns-yadm".yadm_signs(callback)
         end,
@@ -42,19 +47,23 @@ return {
             end)
 
             -- Actions
-            map('n', '<leader>hs', gitsigns.stage_hunk)
-            map('n', '<leader>hr', gitsigns.reset_hunk)
-            map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-            map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-            map('n', '<leader>hS', gitsigns.stage_buffer)
-            map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-            map('n', '<leader>hR', gitsigns.reset_buffer)
-            map('n', '<leader>hp', gitsigns.preview_hunk)
-            map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
-            map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-            map('n', '<leader>hd', gitsigns.diffthis)
-            map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-            map('n', '<leader>td', gitsigns.toggle_deleted)
+            map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "[H]unk [S]tage" })
+            map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "[H]unk [R]eset" })
+            map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+                { desc = "[H]unk [S]tage" })
+            map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+                { desc = "[H]unk [R]eset" })
+            map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Stage Buffer" })
+            map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "[H]unk Stage [U]ndo" })
+            map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "[H]unk [R]eset Buffer" })
+            map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "[H]unk [P]review" })
+            map('n', '<leader>hb', function()
+                gitsigns.blame_line { full = true }
+            end, { desc = "Blame Line" })
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "[T]oggle [B]lame Line" })
+            map('n', '<leader>hd', gitsigns.diffthis, { desc = "Diff This" })
+            map('n', '<leader>hD', function() gitsigns.diffthis('~') end, { desc = "Diff this ~" })
+            map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "[T]oggle [D]eleted" })
 
             -- Text object
             map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')

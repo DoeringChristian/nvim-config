4e0156a0aa8a1b76eaa091d2ec09d0a661d7d185
Christian Döring <christian.doering@tum.de>
2024-10-09 09:20:44 +0200

    Bufferline catppuccin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,28 +1,45 @@
 return {
     "akinsho/bufferline.nvim",
-    tag = "v4.6.1",
+    tag = "v4.7.0",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
     event = "VeryLazy",
-    opts = {
-        options = {
-            diagnostics = "nvim_lsp",
-            diagnostics_indicator = function(count, level, diag, context)
-                local icons = require "config.icons".diagnostics
-                local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-                    .. (diag.warning and icons.Warn .. diag.warning or "")
-                return vim.trim(ret)
-            end,
-            always_show_bufferline = true,
-            offsets = {
-                {
-                    filetype = "neo-tree",
-                    text = "Neo-tree",
-                    highlight = "Directory",
-                    text_align = "left",
+    config = function()
+        local mocha = require("catppuccin.palettes").get_palette "mocha"
+        require "bufferline".setup {
+            highlights = require("catppuccin.groups.integrations.bufferline").get {
+                styles = { "italic", "bold" },
+                custom = {
+                    all = {
+                        fill = { bg = "#000000" },
+                    },
+                    mocha = {
+                        background = { fg = mocha.text },
+                    },
+                    latte = {
+                        background = { fg = "#000000" },
+                    },
                 },
             },
-        },
-    },
+            options = {
+                diagnostics = "nvim_lsp",
+                diagnostics_indicator = function(count, level, diag, context)
+                    local icons = require "config.icons".diagnostics
+                    local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+                        .. (diag.warning and icons.Warn .. diag.warning or "")
+                    return vim.trim(ret)
+                end,
+                always_show_bufferline = true,
+                offsets = {
+                    {
+                        filetype = "neo-tree",
+                        text = "Neo-tree",
+                        highlight = "Directory",
+                        text_align = "left",
+                    },
+                },
+            },
+        }
+    end
 }

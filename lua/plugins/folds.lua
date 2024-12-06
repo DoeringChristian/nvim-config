5aeebc623a7de4c228396ef43ebf0f06cf9a25eb
Christian Döring <christian.doering@tum.de>
2024-12-06 14:41:23 +0100

    Default nvim folding

--- lua/plugins/folds.lua
+++ lua/plugins/folds.lua
@@ -1,13 +1,14 @@
 return {
     {
         "kevinhwang91/nvim-ufo",
+        enabled = true,
         dependencies = {
             "kevinhwang91/promise-async"
         },
         lazy = false,
         keys = {
-            { "zR", function() require "ufo".openAllFolds() end,  desc = "[R]eveal all Folds" },
-            { "zM", function() require "ufo".closeAllFolds() end, desc = "Close all Folds" },
+            -- { "zR", function() require "ufo".openAllFolds() end,  desc = "[R]eveal all Folds" },
+            -- { "zM", function() require "ufo".closeAllFolds() end, desc = "Close all Folds" },
         },
         config = function()
             -- local group = vim.api.nvim_create_augroup("ufo_hover", {})

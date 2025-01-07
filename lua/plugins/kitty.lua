a84f75434958201c43ef57cbb84d7508b45d6c3b
Christian Döring <christian.doering@tum.de>
2025-01-07 18:32:45 +0100

    Reverted on pinning kitty-scrollback

--- lua/plugins/kitty.lua
+++ lua/plugins/kitty.lua
@@ -3,7 +3,8 @@ return {
         "mikesmithgh/kitty-scrollback.nvim",
         enabled = true,
         lazy = true,
-        tag = "v6.1.2", -- https://github.com/mikesmithgh/kitty-scrollback.nvim/issues/282
+        -- tag = "v6.1.2", -- https://github.com/mikesmithgh/kitty-scrollback.nvim/issues/282
+        -- NOTE: use nvim >= v0.11.0-dev-1490+ge27f7125d6
         cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
         event = { "User KittyScrollbackLaunch" },
         -- version = '*', -- latest stable version, may have breaking changes if major version changed

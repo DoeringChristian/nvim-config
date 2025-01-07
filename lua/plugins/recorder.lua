a32cf2b5dd5e88617744f4005d500621f17c7225
Christian Döring <christian.doering@tum.de>
2025-01-07 17:34:13 +0100

    Fixed an issue with nvim-recorder

--- lua/plugins/recorder.lua
+++ lua/plugins/recorder.lua
@@ -2,7 +2,7 @@ return {
     "chrisgrieser/nvim-recorder",
     opts = {
         mapping = {
-            addBreakPoint = "@@" -- ⚠️ this should be a string you don't use in insert mode during a macro
+            -- addBreakPoint = "@@" -- ⚠️ this should be a string you don't use in insert mode during a macro
         }
     },
 }

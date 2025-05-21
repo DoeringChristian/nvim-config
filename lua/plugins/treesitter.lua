dde0c139c0a1da960ea768cdd43307ebe470d4c5
Christian Döring <christian.doering@tum.de>
2025-05-21 16:01:35 +0200

    Removed org from treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,7 +31,6 @@ return {
                     "norg",
                     "ocaml",
                     "ocamllex",
-                    "org",
                     "python",
                     "regex",
                     "rst",

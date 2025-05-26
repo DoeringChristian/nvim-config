9fb6e6b3b6e56fc75f2bb6178f043d594c55a54e
Christian Döring <christian.doering@tum.de>
2025-05-26 21:21:21 +0200

    Enabled harper for markdown

--- lua/config/lsp/settings/harper_ls.lua
+++ lua/config/lsp/settings/harper_ls.lua
@@ -1,3 +1,3 @@
 return {
-    filetypes = { "c", "cpp", "rust", "python", "lua", "toml", "java-script", "restructuredtext" },
+    filetypes = { "c", "cpp", "rust", "python", "lua", "toml", "java-script", "restructuredtext", "markdown" },
 }

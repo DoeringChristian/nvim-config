08030c815cb4ef85b9beb90b813b771cae660f64
Christian Döring <christian.doering@tum.de>
2025-05-26 10:51:20 +0200

    Pinned mason-lspconfig to version and update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,73 +1,76 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
+    {
         "williamboman/mason.nvim",
-        {
+        dependencies = {
+            "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
-            -- Pinned for now, to prevent errors in python
-            -- TODO: test in a few weeks
-            commit = "1a31f82",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            "jay-babu/mason-nvim-dap.nvim",
+            "hrsh7th/cmp-nvim-lsp", -- lsp completions
         },
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "jay-babu/mason-nvim-dap.nvim",
-        "hrsh7th/cmp-nvim-lsp", -- lsp completions
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
+        config = function()
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
 
-        mason.setup {}
-        require "mason-tool-installer".setup {
-            run_on_start = false,
-            ensure_installed = {
-                "ast-grep",
-                "basedpyright",
-                "black",
-                "clang-format",
-                "clangd",
-                "cmake-language-server",
-                "cmakelang",
-                "cmakelint",
-                "codelldb",
-                "esbonio",
-                "glsl_analyzer",
-                "json-lsp",
-                "latexindent",
-                "ltex-ls",
-                "lua-language-server",
-                "markdown-oxide",
-                "matlab-language-server",
-                "nickel-lang-lsp",
-                "alejandra",
-                "pest-language-server",
-                "prettier",
-                "prettierd",
-                "ruff",
-                "rust-analyzer",
-                "texlab",
-                "wgsl-analyzer",
-                "taplo",
-                "harper-ls",
-                "tinymist",
-                "nil",
-                -- "typst-lsp",
-                "typstfmt",
+            mason.setup {}
+            require "mason-tool-installer".setup {
+                run_on_start = false,
+                ensure_installed = {
+                    "ast-grep",
+                    "basedpyright",
+                    "black",
+                    "clang-format",
+                    "clangd",
+                    "cmake-language-server",
+                    "cmakelang",
+                    "cmakelint",
+                    "codelldb",
+                    "esbonio",
+                    "glsl_analyzer",
+                    "json-lsp",
+                    "latexindent",
+                    "ltex-ls",
+                    "lua-language-server",
+                    "markdown-oxide",
+                    "matlab-language-server",
+                    "nickel-lang-lsp",
+                    "alejandra",
+                    "pest-language-server",
+                    "prettier",
+                    "prettierd",
+                    "ruff",
+                    "rust-analyzer",
+                    "texlab",
+                    "wgsl-analyzer",
+                    "taplo",
+                    "harper-ls",
+                    "tinymist",
+                    "nil",
+                    -- "typst-lsp",
+                    "typstfmt",
+                }
             }
-        }
-        mason_lspconfig.setup {
-            automatic_installation = true,
-        }
+            mason_lspconfig.setup {
+                automatic_installation = true,
+            }
 
-        -- Enable this if we want auto-setup
-        -- -- LSP Setup Handlers:
-        -- local function lsp_default_handler(server_name)
-        --     require("lspconfig")[server_name].setup(require("plugins.lsp.handlers").config(server_name))
-        -- end
-        --
-        -- mason_lspconfig.setup_handlers({
-        --     lsp_default_handler,
-        --     ["rust_analyzer"] = function() end, -- NOTE: When removing rust-tools or rustaceanvim remove this as well
-        --     ["ltex"] = function() end,
-        -- })
-    end,
+            -- Enable this if we want auto-setup
+            -- -- LSP Setup Handlers:
+            -- local function lsp_default_handler(server_name)
+            --     require("lspconfig")[server_name].setup(require("plugins.lsp.handlers").config(server_name))
+            -- end
+            --
+            -- mason_lspconfig.setup_handlers({
+            --     lsp_default_handler,
+            --     ["rust_analyzer"] = function() end, -- NOTE: When removing rust-tools or rustaceanvim remove this as well
+            --     ["ltex"] = function() end,
+            -- })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        -- Pinned for now, to prevent errors in python
+        -- TODO: test in a few weeks
+        version = "1.*",
+    },
 }

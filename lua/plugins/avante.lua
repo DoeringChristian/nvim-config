51e23ab55b4676fe413a533f885770f03f52f4f3
Christian Döring <christian.doering@tum.de>
2025-06-16 17:24:23 +0200

    Added avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        build = "make",
+        opts = {
+            auto_suggestions_provider = "ollama-mini",
+            providers = {
+                claude = {
+                    endpoint = "https://api.anthropic.com",
+                    model = "claude-sonnet-4-20250514",
+                    api_key_name = "ANTHROPIC_API_KEY",
+                    timeout = 600000, -- 10 minutes
+                    extra_request_body = {
+                        max_tokens = 32000,
+                    },
+                },
+                ollama = {
+                    endpoint = "http://localhost:11434",
+                    model = "qwq:32b",
+                },
+                ["ollama-mini"] = {
+                    __inherited_from = "ollama",
+                    model = "qwen2.5-coder:1.5b",
+                },
+            }
+        }
+    }
+}

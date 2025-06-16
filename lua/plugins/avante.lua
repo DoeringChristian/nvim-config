6f68d967ef72c37fb2e721cf46aed86593aad185
Christian Döring <christian.doering@tum.de>
2025-06-16 17:44:57 +0200

    Added avante behaviour config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -26,8 +26,18 @@ return {
                 },
             },
             behaviour = {
+                auto_focus_sidebar = false,
+                auto_suggestions = false,
+                auto_set_highlight_group = true,
                 auto_set_keymaps = false,
-            }
+                auto_apply_diff_after_generation = false,
+                support_paste_from_clipboard = false,
+                minimize_diff = false,
+                enable_token_counting = true,
+                enable_cursor_planning_mode = false,
+                use_cwd_as_project_root = true,
+                enable_claude_text_editor_tool_mode = true,
+            },
         }
     }
 }

b0187d6befbdc61089a0662a841bcc39feaff0e2
Christian Döring <christian.doering@tum.de>
2025-07-21 14:18:45 +0200

    Ltex plus fix

--- lua/config/lsp/settings/ltex_plus.lua
+++ lua/config/lsp/settings/ltex_plus.lua
@@ -2,7 +2,16 @@ return {
     filetypes = { "tex", "markdown", "md" },
     flags = { debounce_text_changes = 500 },
     settings = {
-        ltex_plus = {
+        ltex = {
+            enabled = { "bibtex", "context", "context.tex", "html", "latex", "markdown", "org", "restructuredtext",
+                "rsweave" },
+            completionEnabled = true,
+            markdown = {
+                nodes = {
+                    FencedCodeBlock = "ignore",
+                    CodeBlock = "ignore",
+                }
+            },
             checkFrequency = "save",
         }
     }

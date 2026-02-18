626bbf49d0f50d1de1a27e045fc3e7c99e905b3f
Christian Döring <christian.doering@tum.de>
2026-02-18 18:48:55 +0100

    Better escape mappings

--- lua/plugins/escape.lua
+++ lua/plugins/escape.lua
@@ -2,7 +2,37 @@ return {
   {
     'max397574/better-escape.nvim',
     config = function()
-      require('better_escape').setup()
+      require('better_escape').setup {
+        default_mapping = false,
+        mappings = {
+          i = {
+            j = {
+              -- These can all also be functions
+              k = '<Esc>',
+            },
+          },
+          c = {
+            j = {
+              k = '<C-c>',
+            },
+          },
+          t = {
+            j = {
+              k = '<C-\\><C-n>',
+            },
+          },
+          v = {
+            j = {
+              k = '<Esc>',
+            },
+          },
+          s = {
+            j = {
+              k = '<Esc>',
+            },
+          },
+        },
+      }
     end,
   },
 }

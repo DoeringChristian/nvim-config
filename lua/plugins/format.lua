6cea9ef6ba647f124c9538089bf9c20b20fa040d
Christian Döring <christian.doering@tum.de>
2026-05-07 17:46:23 +0200

    Bibtex with latexindent

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -104,6 +104,7 @@ return {
           nix = { 'alejandra' },
           yaml = { 'prettier' },
           tex = { 'latexindent', lsp_format = 'never' },
+          bib = { 'latexindent', lsp_format = 'never' },
         },
         format_after_save = function(bufnr)
           if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
